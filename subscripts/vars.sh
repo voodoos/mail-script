caab3e3f7783a6eed20dc86ee1c1fed33065ca15
Ulysse <VooDooS@users.noreply.github.com>
2017-11-20 15:14:43 +0100

    Update vars.sh

--- subscripts/vars.sh
+++ subscripts/vars.sh
@@ -10,45 +10,48 @@ export MAIL_VSERV_ROOT=${WEB_PATH}/mail
 ##########
 
 #echo -n "Hello ! What's your name ? (used to configure default mysql and postfixadmin accounts, you can enter the same as your UNIX username)"
-export USERP=ulysse
+export USERP=
 
 #echo -n "Choose a nice hostname for your server (will be append in front of your domain name): "
-export HOSTNAME=polypheme
+export HOSTNAME=
 
 #echo -n "Enter ssh port to be allowed in UFW: "
 export SSH_PORT=22
 
 #echo -n "Enter your domain name (without the last part .fr .org etc): "
-DOMAIN_NAME=cyclo
+DOMAIN_NAME=
 #echo -n "Enter the last part of your domain name (fr org etc): "
-DOMAIN_EXT=pe
+DOMAIN_EXT=
 
 export DOMAIN_NAME
 export DOMAIN_EXT
 export DOMAIN=${DOMAIN_NAME}.${DOMAIN_EXT}
+export MAILDOMAIN=mail.${DOMAIN
+export FQDN=${HOSTNAME}.${DOMAIN}
 
 #############
 #  MYSQL VARS
 ##########
-#echo -n "Choose a strong root password for the database: "
-MYSQL_ROOT_PASS=totoro
+#echo -n "Choose a strong root password for the database : "
+MYSQL_ROOT_PASS=
 
+# Choose a name for your database user:
 export DB_USER=$USERP
 #echo -n "Choose a password for your personnal dbuser (named after your unix account $USER): "
-export MYSQL_USER_PASS=$MYSQL_ROOT_PASS
-export MYSQL_USER=$USERP
+export MYSQL_USER_PASS=
+export MYSQL_USER=${DB_USER}
 
 #echo -n "Choose a password for postfix's mysql user (strong, no need to remember): "
-export MYSQL_POSTFIX_PASS=$MYSQL_ROOT_PASS
+export MYSQL_POSTFIX_PASS=
 
 #echo -n "Choose a password for roundcube's mysql user (strong, no need to remember): "
-export MYSQL_RC_PASS=$MYSQL_ROOT_PASS
+export MYSQL_RC_PASS=${MYSQL_POSTFIX_PASS}
 
 #############
 #  POSTFIXADMIN VARS
 ##########
 #echo -n "Chosse a strong password that you can remenber for your main postfixadmin account"
-export PFADMIN_PASS=$MYSQL_ROOT_PASS
+export PFADMIN_PASS=
 
 #############
 #  DOVECOT VARS
@@ -63,14 +66,15 @@ export DV_ROUNDS=400000
 export RSPAMD_DIR="/etc/rspamd"
 export RSPAMD_CONF_DIR=${RSPAMD_DIR}/local.d
 
-#echo -n "Chosse a strong password for rspamd webui"
-export RSPAMD_PASS=$MYSQL_ROOT_PASS
+#echo -n "Choose a strong password for rspamd webui"
+export RSPAMD_PASS=
 
 
 
 bash subscripts/print.sh  "Make sure your DNS are properly configured :"
-echo "$DOMAIN_NAME.$DOMAIN_EXT must have a A record pointing to your server"
-echo "mail.$DOMAIN_NAME.$DOMAIN_EXT must have a A record pointing to your server"
+echo "${DOMAIN} must have a A record pointing to your server"
+echo "${MAILDOMAIN} must have a A record pointing to your server"
+echo "${FQDN} must have a A record pointing to your server"
 echo
 echo -n "Are you ready ? then pres ENTER !"
 read R
